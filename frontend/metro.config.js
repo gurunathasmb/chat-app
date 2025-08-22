ce1b5a382a3d8ddfba500f22e2f3292c4ef7a43d
gurunathasmb <gurunathagoudambiradar@gmail.com>
2025-08-22 16:21:30 +0530

    every thing

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -1,2 +1,17 @@
 const { getDefaultConfig } = require('expo/metro-config');
-module.exports = getDefaultConfig(__dirname);
+const path = require('path');
+
+module.exports = (async () => {
+  const config = await getDefaultConfig(__dirname);
+
+  // Normalize paths to forward slashes (fixes %5C on Windows)
+  config.resolver.assetExts = config.resolver.assetExts || [];
+  config.resolver.sourceExts = config.resolver.sourceExts || [];
+
+  // Ensure paths use /
+  config.watchFolders = config.watchFolders.map((folder) =>
+    folder.split(path.sep).join(path.posix.sep)
+  );
+
+  return config;
+})();
